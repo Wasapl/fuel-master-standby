84cc91dfa78e7e5e1274c7b7d50924e1b2c769d4
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2016-10-19 16:03:44 -0700

    implement restore.sh

--- restore.sh
+++ restore.sh
@@ -11,11 +11,39 @@ if [ $? -eq 1 ]; then
 fi
 
 #check latest backup exist
+LAST_DIR=`ls -t $STANDBY_DIR -1|head -n 1`
+if ! [ -d $LAST_DIR ]; then
+    echo "Unexpected files in $STANDBY_DIR. Aborting."
+    exit 1
+fi
+if ! [ -s $DIR_BACKUP/$LAST_DIR/$STATE_FILE ]; then
+    echo "File $DIR_BACKUP/$LAST_DIR/$STATE_FILE do not exist. Aborting."
+    exit 1
+fi
+if ! [ -s $DIR_BACKUP/$LAST_DIR/$REPO_FILE ]; then
+    echo "File $DIR_BACKUP/$LAST_DIR/$REPO_FILE do not exist. Aborting."
+    exit 1
+fi
+
 
 #change astute.yaml in tarball
+if [ -s astute/astute.yaml]; then
+    rm astute/astute.yaml
+fi
+mkdir astute 2>/dev/null
+
+tar -Oxzf $DIR_BACKUP/$LAST_DIR/$STATE_FILE astute/astute.yaml | python -c "import sys;import yaml; data = yaml.load(sys.stdin);data['HOSTNAME']=\"${STANDBY_HOSTNAME}\"; yaml.dump(data, sys.stdout);"  >astute/astute.yaml
+tar -rzf $DIR_BACKUP/$LAST_DIR/$STATE_FILE astute/astute.yaml
+if [ -s astute/astute.yaml ]; then
+    tar -uzf $DIR_BACKUP/$LAST_DIR/$STATE_FILE astute/astute.yaml
+else
+    echo "Astute.yaml modify was unsuccessful. Aborting."
+    exit 1
+fi 
+
 
 #restore backup
-octane fuel-restore --from $DIR_BACKUP/$STATE_FILE
-octane fuel-repo-restore --from $DIR_BACKUP/$REPO_FILE
+octane fuel-restore --from $DIR_BACKUP/$LAST_DIR/$STATE_FILE
+octane fuel-repo-restore --from $DIR_BACKUP/$LAST_DIR/$REPO_FILE
 
 #delete backup files (optionaly)
