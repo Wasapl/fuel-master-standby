7c35116d767468dff3e98c724f01308714f43f04
Vasiliy Pleshakov <vpleshakov@mirantis.com>
2016-10-20 14:53:08 -0700

    Making it work

--- backup.sh
+++ backup.sh
@@ -1,14 +1,30 @@
-#!/bin/bash -x
+#!/bin/bash
 
-ROOT=$(dirname `readlink -f $0`)
-source $ROOT/config.sh
+BACKUP_DIR=/var/backup/octane/
+STANDBY_IP=67.217.83.5
+STANDBY_DIR=/var/backup/octane/
+MASTER_HOSTNAME=fuel1
+STANDBY_HOSTNAME=fuel2
+DATE=`date +%F`
+
+STATE_FILE=fuel-backup.8.0.tar.gz
+REPO_FILE=fuel-repo-backup.8.0.tar.gz
+
+LOG_FILE=/var/log/master_standby.log
+
+exec 1<&-
+exec 2<&-
+exec 1<>$LOG_FILE
+exec 2>&1
 
 #TODO check octane installed
+set +e
 type octane >/dev/null 2>&1
 if [ $? -eq 1 ]; then
     echo 'There is no fuel-octane. Run "yum install fuel-octane" first.'
     exit 1
 fi
+set -e
 
 if [ -d $BACKUP_DIR ]; then
     # should check content of BACKUP_DIR prior to run next command 
@@ -26,7 +42,9 @@ fi
 octane fuel-backup --to $BACKUP_DIR/$STATE_FILE
 octane fuel-repo-backup --full --to $BACKUP_DIR/$REPO_FILE
 
-
+echo 'Copying files to standby node...'
 ssh $STANDBY_IP "mkdir -p /$STANDBY_DIR/$DATE/"
 scp $BACKUP_DIR/$STATE_FILE $STANDBY_IP:/$STANDBY_DIR/$DATE/
 scp $BACKUP_DIR/$REPO_FILE $STANDBY_IP:$STANDBY_DIR/$DATE/
+
+echo 'Done'
--- restore.sh
+++ restore.sh
@@ -1,14 +1,32 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-ROOT=$(dirname `readlink -f $0`)
-source $ROOT/config.sh
+BACKUP_DIR=/var/backup/octane/
+STANDBY_IP=67.217.83.5
+STANDBY_DIR=/var/backup/octane/
+MASTER_HOSTNAME=fuel1
+STANDBY_HOSTNAME=fuel2
+PASSWORD=
+DATE=`date +%F`
+
+STATE_FILE=fuel-backup.8.0.tar.gz
+REPO_FILE=fuel-repo-backup.8.0.tar.gz
+
+LOG_FILE=/var/log/master_standby.log
+
+
+exec 1<&-
+exec 2<&-
+exec 1<>$LOG_FILE
+exec 2>&1
 
 #TODO check octane installed
+set +e
 type octane >/dev/null 2>&1
 if [ $? -eq 1 ]; then
     echo 'There is no fuel-octane. Run "yum install fuel-octane" first.'
     exit 1
 fi
+set -e
 
 #check latest backup exist
 LAST_DIR=`ls -t $STANDBY_DIR -1|head -n 1`
@@ -27,10 +45,12 @@ fi
 
 
 #change astute.yaml in tarball
-if [ -s astute/astute.yaml]; then
+if [ -s astute/astute.yaml ]; then
     rm astute/astute.yaml
 fi
-mkdir astute 2>/dev/null
+if ! [ -d astute ]; then
+    mkdir astute 2>/dev/null
+fi
 
 tar -Oxzf $STANDBY_DIR/$LAST_DIR/$STATE_FILE astute/astute.yaml | python -c "import sys;import yaml; data = yaml.load(sys.stdin);data['HOSTNAME']=\"${STANDBY_HOSTNAME}\"; yaml.dump(data, sys.stdout);"  >astute/astute.yaml
 
@@ -42,11 +62,11 @@ if [ -s astute/astute.yaml ]; then
 else
     echo "Astute.yaml modify was unsuccessful. Aborting."
     exit 1
-fi 
+fi
 
 
 #restore backup
-octane fuel-restore --from $STANDBY_DIR/$LAST_DIR/$STATE_FILE --admin-password <>
+octane fuel-restore --from $STANDBY_DIR/$LAST_DIR/$STATE_FILE --admin-password $PASSWORD
 octane fuel-repo-restore --from $STANDBY_DIR/$LAST_DIR/$REPO_FILE
 
 #delete backup files (optionaly)
